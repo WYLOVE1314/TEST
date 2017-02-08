b9aa7520c449ac8805fd479333f0de004d6de78f
WYLOVE1314 <wangyang07270817@163.com>
2017-02-08 15:31:53 +0800

    单例模式--静态内部类
    单例模式--静态内部类

---
+++ Singleton2.java
@@ -0,0 +1,14 @@
+package cn.ce.ebiz;
+
+public class Singleton2 {
+	
+	private static class SingletonHolder{
+		private static final Singleton2 INSTANCE = new Singleton2();
+	}
+	
+	private Singleton2(){}
+	
+	public static final Singleton2 getInstance(){
+		return SingletonHolder.INSTANCE;
+	}
+}
